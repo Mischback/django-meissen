9b888cd6b8085ce85c7136962e72b3463731179e
Mischback <dev@mischback.de>
2015-02-16 14:31:13 +0100

    Finished signal test for RepositoryLocation pre_save-signal

--- meissen/tests/models/repository_location.py
+++ meissen/tests/models/repository_location.py
@@ -1,13 +1,55 @@
 """Contains tests for model RepositoryLocation"""
 
 # Python imports
+from os import chmod
+from shutil import rmtree
+from tempfile import mkdtemp
 from unittest import skip
 
 # app imports
+from meissen.exceptions import *
+from meissen.models import RepositoryLocation
 from meissen.tests import MeissenModelTestCase
 
 
 class MeissenRepositoryLocationModelTestCase(MeissenModelTestCase):
 
+    def setUp(self):
+        self.filesystem_path = mkdtemp()
+
+    def tearDown(self):
+        rmtree(self.filesystem_path)
+
     def test_unique_path(self):
+        """Test the uniqueness of the path
+
+        Meaning: There must not be two RepositoryLocation objects with
+        identical path attributes. This is enforced by using Django's CharField
+        unique attribute. Let's just see if this works.
+        """
         skip
+
+    def test_enforce_trailing_slash(self):
+        """The path attribute must have a trailing slash."""
+        skip
+
+    def test_signal_check_repo_location(self):
+        """Test the correctness of the callback functions."""
+        # test existence
+        with self.assertRaises(MeissenNotExistingPathException):
+            a = RepositoryLocation.objects.create(path='meissen{0}'.format(self.filesystem_path))
+
+        # readable
+        chmod(self.filesystem_path, 0000)
+        with self.assertRaises(MeissenNoReadAccessException):
+            a = RepositoryLocation.objects.create(path=self.filesystem_path)
+
+        # writeable
+        chmod(self.filesystem_path, 0400)
+        with self.assertRaises(MeissenNoWriteAccessException):
+            a = RepositoryLocation.objects.create(path=self.filesystem_path)
+
+        # executable
+        chmod(self.filesystem_path, 0600)
+        with self.assertRaises(MeissenNoExecuteAccessException):
+            a = RepositoryLocation.objects.create(path=self.filesystem_path)
