8b88a9ea7f44a694270eff7ca97e61a5a05d7daa
Mischback <dev@mischback.de>
2015-02-16 20:43:24 +0100

    Created overview view
    All adjustments beside views/basic.py are only for completeness.

--- meissen/views/basic.py
+++ meissen/views/basic.py
@@ -3,7 +3,21 @@
 # Django imports
 from django.shortcuts import render
 
+# app imports
+from meissen.models import Repository
+
 
 def home(req):
     """The app's landing page"""
     return render(req, 'meissen/home.html')
+
+def overview(req):
+    """The app's landing page"""
+
+    # get all repositories
+    # TODO: Here is the place to limit access
+    repos = Repository.objects.all()
+
+    return render(req, 'meissen/overview.html',
+        { 'repos': repos, },
+    )
