9a423fedbce63200b05d1573e7065e991a980499
Mischback <dev@mischback.de>
2015-02-16 10:57:15 +0100

    RepositoryLocation created

---
+++ meissen/models/repository.py
@@ -0,0 +1,20 @@
+"""These classes represent repositories irl"""
+
+# Django imports
+from django.db import models
+
+class RepositoryLocation(models.Model):
+    """Represents a filesystem location where repositories are stored"""
+
+    path = models.CharField(max_length=255, unique=True, default='/tmp/')
+    """The file system path (given from /)"""
+    # TODO: enforce trailing slash!
+    # TODO: Windows compatibility?!
+
+    def __unicode__(self):
+        return '[{0}] {1}'.format(self._meta.verbose_name, self.path)
+
+    class Meta:
+        app_label = 'meissen'
+        verbose_name = 'RepositoryLocation'
+        verbose_name_plural = 'RepositoryLocations'
