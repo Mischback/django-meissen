174d444af6788daed61a024c553b076e2a6e8c0c
Mischback <dev@mischback.de>
2015-02-27 23:18:06 +0100

    Working on repo overview

--- meissen/views/repository.py
+++ meissen/views/repository.py
@@ -1,9 +1,15 @@
 """Contains views for a single repo"""
 
 # Django imports
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+# app imports
+from meissen.models import Repository
 
 
 def overview(req, repo_id):
-    """The app's landing page"""
+    """The repository's landing page"""
+
+    repo = get_object_or_404(Repository, pk=repo_id)
+
     return render(req, 'meissen/repository/overview.html')
