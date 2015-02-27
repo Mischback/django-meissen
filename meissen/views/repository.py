fd4eccc8353edb7a2c36c5f6f74dccf400826b6a
Mischback <dev@mischback.de>
2015-02-27 22:17:54 +0100

    Created repository overview template

--- meissen/views/repository.py
+++ meissen/views/repository.py
@@ -6,4 +6,4 @@ from django.shortcuts import render
 
 def overview(req, repo_id):
     """The app's landing page"""
-    return render(req, 'meissen/home.html')
+    return render(req, 'meissen/repository/overview.html')
